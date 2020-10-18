e4b7da3dab2c52a561f305b0a49364f31bd8c1e2
Paul <46460533+resetmedia@users.noreply.github.com>
2020-10-18 16:35:17 -0400

    Image Optimization

--- js/all.js
+++ js/all.js
@@ -219,3 +219,65 @@ jQuery(document).ready(function(){
     $('.sub_overlay').removeClass('visible');
   });
 });
+
+jQuery(document).ready(function($){
+
+	var granimInstance = new Granim({
+		element: '#canvas-basic',
+		direction: 'left-right',
+		isPausedWhenNotInView: true,
+		states : {
+			"default-state": {
+				gradients: [
+					['#ff9966', '#ff5e62'],
+					['#00F260', '#0575E6'],
+					['#e1eec3', '#f05053']
+				]
+			}
+		}
+	});
+
+	/*$('#subscribe-form').submit(function(e){
+		e.preventDefault();
+		var data = {
+			'action': 'mail_before_submit',
+			'security': rsm_ajax.security,
+			'name': $(this).find('input[name="name"]').val(),
+			'email': $(this).find('input[name="email"]').val(),
+			'message': $(this).find('textarea[name="message"]').val()
+		};
+		$.ajax({
+			type: "POST",
+			url: rsm_ajax.ajaxurl,
+			data: data,
+			success: function(){}
+		});
+
+		$(this).find('.input-submit').text('THANK YOU').attr('disabled', 'disabled');;
+
+		return false;
+	});
+
+	$('#contact_form').submit(function(e){
+		e.preventDefault();
+		var data = {
+			'action': 'mail_before_submit',
+			'security': rsm_ajax.security,
+			'name': $(this).find('input[name="name"]').val(),
+			'email': $(this).find('input[name="email"]').val(),
+			'message': $(this).find('textarea[name="message"]').val()
+		};
+		$.ajax({
+			type: "POST",
+			url: rsm_ajax.ajaxurl,
+			data: data,
+			success: function(){}
+		});
+
+		$(this).find('.input-submit').text('Thank You').attr('disabled', 'disabled');
+
+		return false;
+	});*/
+});
+
+
